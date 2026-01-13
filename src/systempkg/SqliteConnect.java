f163545368064f3b4fd2eae7d05bcfd5780c3d61
kimdongju <dk6249@gmail.com>
2026-01-13 16:27:47 +0900

    콘솔로 종족 기본 스텟 표시 확인 완료.

--- src/systempkg/SqliteConnect.java
+++ src/systempkg/SqliteConnect.java
@@ -2,29 +2,16 @@ package systempkg;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 public class SqliteConnect {
-	public static void main(String[] args) {
 		
 		// SQLite JDBC URL
-		String url = "jdbc:sqlite:resource/db/PCFPdb.db";
+		private static final String URL = "jdbc:sqlite:resource/db/PCFPdb.db";
 		
-		// 1. test connected
-		try (Connection conn = DriverManager.getConnection(url)){
-			System.out.println("✅ SQLite DataBase Connected Success.");
-			// 2. simple query test
-			try (Statement stmt = conn.createStatement()){
-				// SQLite Version Checker
-				ResultSet rs = stmt.executeQuery("SELECT sqlite_version();");
-				if (rs.next()) {
-					System.out.println("SQLite Version: " + rs.getString(1));
-				}
-			}
-		} catch (Exception e) {
-			System.out.println("❌ SQlite DataBase Connect Fail.");
-			e.printStackTrace();
+		// instance prevention
+		private SqliteConnect() {}
+		
+		public static Connection getConnection() throws Exception {
+			return DriverManager.getConnection(URL);
 		}
-	}
 }
---
+++ src/testbuild/RaceBasicInfoLoad.java
@@ -0,0 +1,79 @@
+package testbuild;
+
+import systempkg.SqliteConnect;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+// After, Change to "RaceInFoDAO"
+public class RaceBasicInfoLoad {
+	public static void main(String[] args) {
+		List<String> RACE_IDs = new ArrayList<>();
+		// 1. Race Index output
+		String listSql = "SELECT RACE_ID FROM Race_Basic_Stats ORDER BY RACE_ID";
+		
+		try (Connection conn = SqliteConnect.getConnection();
+			 PreparedStatement ps = conn.prepareStatement(listSql);
+			 ResultSet rs = ps.executeQuery()) {
+			System.out.println("===Race Index===");
+			int index = 1;
+			while (rs.next()) {
+				String Key = rs.getString("RACE_ID");
+				RACE_IDs.add(Key);
+				System.out.println(index + ". " + Key);
+				index++;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		if(RACE_IDs.isEmpty()) {
+			System.out.println("There is no race data.");
+			return;
+		}
+		
+		// 2. User output
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Selecte Race number: ");
+		int choice = sc.nextInt();
+		
+		if (choice < 1 || choice > RACE_IDs.size()) {
+			System.out.println("It's a wrong choice.");
+			return;
+		}
+		
+		String selectedRaceID = RACE_IDs.get(choice-1);
+		
+		// 3. Selected Race Stats output
+		
+		String dataSql =
+				"SELECT RACE_STR, RACE_CON, RACE_AGI, RACE_DEX, RACE_INT, RACE_WIS, RACE_LUK " +
+				"FROM Race_Basic_Stats WHERE RACE_ID = ?";
+		
+		try (Connection conn = SqliteConnect.getConnection();
+			 PreparedStatement ps = conn.prepareStatement(dataSql)) {
+			
+			ps.setString(1, selectedRaceID);
+			ResultSet rs = ps.executeQuery();
+			
+			if (rs.next()) {
+				System.out.println("\n[" + selectedRaceID + " Stats]");
+				System.out.println("STR: " + rs.getInt("RACE_STR"));
+				System.out.println("CON: " + rs.getInt("RACE_CON"));
+				System.out.println("AGI: " + rs.getInt("RACE_AGI"));
+				System.out.println("DEX: " + rs.getInt("RACE_DEX"));
+				System.out.println("INT: " + rs.getInt("RACE_INT"));
+				System.out.println("WIS: " + rs.getInt("RACE_WIS"));
+				System.out.println("LUK: " + rs.getInt("RACE_LUK"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
