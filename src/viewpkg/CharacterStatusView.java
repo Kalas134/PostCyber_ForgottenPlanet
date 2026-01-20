3f37099102fdf832ce49905c48b813e2a6bd1e28
kimdongju <dk6249@gmail.com>
2026-01-20 21:55:34 +0900

    CharacterStatusView를 변형하여 최종 파생 스텟까지 포함하여 반영 완료.

--- src/viewpkg/CharacterStatusView.java
+++ src/viewpkg/CharacterStatusView.java
@@ -2,10 +2,15 @@ package viewpkg;
 
 import java.util.List;
 import vopkg.CharacterFinalStatsVO;
+import vopkg.CharacterCombatStatsVO;
+import vopkg.CharacterSurvivalStatsVO;
+
+import systempkg.CharacterDeriverdStatsCalculator;
+
 
 public class CharacterStatusView {
 	public static void printCharacters(List<CharacterFinalStatsVO> characters) {
-		System.out.println("===CHARACTER BASE STATUS LIST===");
+		System.out.println("===CHARACTER STATUS LIST===");
 		
 		for (CharacterFinalStatsVO vo : characters) {
 			printSingleCharacter(vo);
@@ -19,7 +24,7 @@ public class CharacterStatusView {
 		System.out.println("EXP: " + vo.getExp());
 		System.out.println("SP: " + vo.getStatPoint() +"\n");
 		
-		System.out.println("[ STATS ]");
+		System.out.println("<[ BASE STATS ]>");
 		System.out.println("STR: " + vo.getFinalStr());
 		System.out.println("CON: " + vo.getFinalCon());
 		System.out.println("AGI: " + vo.getFinalAgi());
@@ -27,5 +32,55 @@ public class CharacterStatusView {
 		System.out.println("INT: " + vo.getFinalInt());
 		System.out.println("WIS: " + vo.getFinalWis());
 		System.out.println("LUK: " + vo.getFinalLuk());
+		
+		System.out.println("\n<[ DERIVERD STATS ]>");
+		
+		// DERIVERD STATS Calculate
+		
+		CharacterCombatStatsVO combat =
+				CharacterDeriverdStatsCalculator.calculateCombat(
+						vo.getFinalStr(), 
+						vo.getFinalCon(), 
+						vo.getFinalAgi(), 
+						vo.getFinalDex(), 
+						vo.getFinalInt(), 
+						vo.getFinalWis(), 
+						vo.getFinalLuk()
+				);
+		CharacterSurvivalStatsVO survival =
+				CharacterDeriverdStatsCalculator.calculateSurvival(
+						vo.getFinalStr(), 
+						vo.getFinalCon(), 
+						vo.getFinalAgi(), 
+						vo.getFinalDex(), 
+						vo.getFinalInt(), 
+						vo.getFinalWis(), 
+						vo.getFinalLuk()
+				);
+		
+		// DERIVERD STATS Print
+		System.out.println("[ COMBAT STATS ]");
+		System.out.println("HP: " + combat.getHp());
+		System.out.println("MP: " + combat.getMp());
+		System.out.println("ATK: " + combat.getAtk());
+		System.out.println("DEF: " + combat.getDef());
+		System.out.println("MAG: " + combat.getMag());
+		System.out.println("RES: " + combat.getRes());
+		System.out.println("CRT: " + combat.getCrt() + "%");
+		System.out.println("CRR: " + combat.getCrr() + "%");
+		System.out.println("P.CRIT MULT: x" + combat.getPcr());
+		System.out.println("M.CRIT MULT: x" + combat.getMcr());
+		System.out.println("SPD: " + combat.getSpd());
+		
+		System.out.println("[ SURVIVAL STATS ]");
+		System.out.println("Stamina: " + survival.getStamina());
+	    System.out.println("Carry Cap: " + survival.getCarryCapacity());
+	    System.out.println("Hunger Res: " + survival.getHungerResistance());
+	    System.out.println("Heal Rate: " + survival.getHealRate());
+	    System.out.println("Immune Str: " + survival.getImmuneStrength());
+	    System.out.println("Crafting Eff: " + survival.getCraftingEfficiency());
+	    System.out.println("Env Tolerance: " + survival.getEnviromentTolerance());
+	    System.out.println("Scavenging: " + survival.getScavenging());
+	    System.out.println("Instinct: " + survival.getInstinct());
 	}
 }
